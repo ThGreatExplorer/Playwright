fec890297cf1f97df035ca622e717076a0ef3a6f
Anfisa Bogdanenko <anfisabogdanenko@proton.me>
2025-09-25 17:11:33 -0400

    Main refactor

---
+++ src/main/scala/AssingmentRunner.scala
@@ -0,0 +1,58 @@
+package main
+
+import sexprs.SExprs._
+import csk.CSKMachine
+import csk.Control
+
+import ParserAST.Parser as ParserAST
+
+enum Result:
+  case Count(n : Int)
+  case ParseError
+  case ParseBelongs
+  case Success(n : Number)
+  case RuntimeError
+
+  def outputString: String = this match
+    case Count(n) => s"\"$n\""
+    case ParseError => "\"parser error\""
+    case ParseBelongs => "\"belongs\""
+    case Success(n) => s"$n"
+    case RuntimeError => "\"runtime error\""
+
+object AssignmentRunner:
+  /**
+    * Result printer for Assignment 3 — Bare Bones: CSK
+    *
+    * @param input SExpr read from stdin
+    */
+  def cskBareBones(input: SExpr): Result = 
+    val prog = ParserAST.parse(input)
+
+    if ParserAST.hasError(prog) then
+      Result.ParseError
+    else
+      CSKMachine.run(prog) match
+        case n: Number => Result.Success(n)
+        case e: Control.Err => Result.RuntimeError
+
+  /**
+    * Result printer for Assignment 2 — Bare Bones: Parser
+    *
+    * @param input SExpr read from stdin
+    */
+  def parserBareBones(input: SExpr): Result = 
+    val prog = ParserAST.parse(input)
+
+    if ParserAST.hasError(prog) then
+      Result.ParseError
+    else
+      Result.ParseBelongs
+
+  /**
+    * Result printer for Assignment 1 — Start Me Up
+    *
+    * @param input SExpr read from stdin
+    */
+  def startUp(input: SExpr): Result =     
+    Result.Count(counter(input))
--- src/main/scala/sexprs/SExprs.scala
+++ src/main/scala/sexprs/SExprs.scala
@@ -1,5 +1,7 @@
 package sexprs
 
+import annotation.tailrec
+
 object SExprs {
 
   sealed trait SExpr extends Positioned
@@ -19,4 +21,24 @@ object SExprs {
   /* SComment is never parsed, only used for pretty printing */
   case class SComment(s: String) extends SExpr
 
+  /** 
+   * Counts number of aNames in a given Example s-expression
+   * Throws an exception if an unexpected SExpr node is found 
+   * Tail-call optimized to handle deep nesting
+   *
+   * For Homework 1
+   * 
+   * @param input SExpr read from stdin
+   * @return integer count of aName elements
+   */
+  def counter(input: SExpr): Int =
+    @tailrec
+    def loop(stack: List[SExpr], acc: Int): Int = 
+    stack match
+        case Nil => acc
+        case SSymbol(_) :: rest => loop(rest, acc + 1)
+        case SDouble(_) :: rest => loop(rest, acc)
+        case SList(elements) :: rest => loop(elements ++ rest, acc)
+        case head :: _ => throw new Exception("SExpr not part of Example Structure: " + head)
+    loop(List(input), 0)
 }
